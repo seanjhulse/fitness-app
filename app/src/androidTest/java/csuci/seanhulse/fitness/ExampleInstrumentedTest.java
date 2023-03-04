671dd438512b8423d0c63a5a921690708c3a776c
seanjhulse <seanjhulse@gmail.com>
2023-03-04 11:10:03 -0800

    Rename remaining vetigials of com.example

--- app/src/androidTest/java/csuci/seanhulse/fitness/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/csuci/seanhulse/fitness/ExampleInstrumentedTest.java
@@ -20,6 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.fitnessform", appContext.getPackageName());
+        assertEquals("csuci.seanhulse.fitness", appContext.getPackageName());
     }
 }
